1f99a180a5d437b58e49cd79ca760c9d421249a3
Shotaro Ooki <shoutaro0726@gmail.com>
2020-05-25 22:39:42 +0900

    Commit

--- vue_study/src/App.vue
+++ vue_study/src/App.vue
@@ -1,30 +1,40 @@
 <template>
-<div>
-  <like-header></like-header>
-  <p>{{ number }}</p>
-  <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
-  <LikeNumber :total-number="number"></LikeNumber>
-</div>
+  <div>
+    <like-header>
+      <h3>はじめまして。</h3>
+    </like-header>
+    <LikeNumber :total-number="number" @my-click="incrementNumber"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>    
+    </keep-alive>
+  </div>
 </template>
 
 <script>
 //ローカル登録
-import LikeHeader from './components/LikeHeader'
+import LikeHeader from "./components/LikeHeader";
+import About from './components/About.vue';
+import Home from './components/Home.vue';
 export default {
   data() {
     return {
-      number: 10
+      number: 10,
+      currentComponent: "Home",
     };
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home,
   },
   methods: {
     incrementNumber(value) {
-      this.number = value
+      this.number = value;
     }
   }
-}
+};
 </script>
 
 <style scoped>
--- vue_study/src/components/LikeHeader.vue
+++ vue_study/src/components/LikeHeader.vue
@@ -1,3 +1,23 @@
 <template>
-    <h1>トータルのいいね数</h1>
+    <div>
+        <slot  :user="user" text="text">
+            <slot></slot>
+            <hr>
+        </slot>
+        <slot name="number"></slot>
+        <slot name="title"></slot>
+    </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            user: {
+                firstName: 'Shotaro',
+                lastName: 'Ooki',
+            }
+        }
+    }
+}
+</script>
