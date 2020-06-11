520836857a87c0db0f8602a70713dde39a796ee4
Shotaro Ooki <shoutaro0726@gmail.com>
2020-06-11 09:26:28 +0900

    Commit

--- vue_study/src/components/Home.vue
+++ vue_study/src/components/Home.vue
@@ -1,3 +1,46 @@
 <template>
-    <p>Home</p>
+<div>
+   <p v-border:dotted.round.shadow="{width: '5px', color: 'black'}">Home</p>
+   <h2>{{ title  | toUpperCase | upperCase }}</h2>
+   <p>{{ subTitle  | toLowerCase  }}</p>
+   <p>{{ number }}</p>
+   <button @click="number++">+1</button>
+   <CountNumber></CountNumber>
+</div>
 </template>
+
+<script>
+import CountNumber from './CountNumber';
+import {tokyoNumber} from '@/tokyoNumber';
+export default {
+  mixins: [tokyoNumber],
+    data() {
+        return {
+        tmpData: "Hello",
+        };
+    },
+    components: {
+      CountNumber
+    },
+    // computed: {
+    //   upperCaseTitle() {
+    //     return this.title.toUpperCase();
+    //   },
+    // },
+
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+
+      if (binding.modifiers.round.shadow) {
+        el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
+      }
+    }
+  }
+};
+</script>
--- vue_study/src/main.js
+++ vue_study/src/main.js
@@ -6,6 +6,28 @@ Vue.config.productionTip = false
 
 //グローバル登録
 Vue.component('LikeNumber', LikeNumber);
+// Vue.component("border",(el,binding) => {
+//   el.style.borderWidth = binding.value.width;
+//   el.style.borderColor = binding.value.color;
+//   el.style.borderStyle = binding.arg;
+//   if (binding.modifiers.round) {
+//     el.style.borderRadius = "0.5rem";
+//   }
+
+//   if (binding.modifiers.round.shadow) {
+//     el.style.boxShadow = "0 2px 5px rgba(0,0,0,0.26)";
+//   }
+// });
+
+Vue.filter("upperCase", (value) => {
+  return value.toUpperCase();
+});
+
+// Vue.mixin({
+//   created() {
+//     console.log("global mixin");
+//   }
+// });
 
 new Vue({
   render: h => h(App),
